a46480215ed8500c2092ade480a1700ca25a0ac4
sanshraychundru <sanshray.chundru@gmail.com>
2024-07-09 20:06:18 -0700

    2nd commit

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -16,7 +16,7 @@ print("Please select operation -\n" \
         "3. Multiply\n" \
         "4. Divide\n")
 
-#comment 2sadsa
+#operation
 select = int(input("Select operations form 1, 2, 3, 4 :"))
  
 number_1 = int(input("Enter first number: "))
