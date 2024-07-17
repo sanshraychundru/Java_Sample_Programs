901996b9e169aee59e919b551ff9526972a65dde
sanshraychundru <sanshray.chundru@gmail.com>
2024-07-17 12:05:06 -0700

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -1,21 +1,32 @@
+import java.util.Scanner;
+
 public class calculator {
 
     public static void main(String [] args ) throws Exception {
-        int  var1  = 9123;
-        int  var2  = 9012;
-        char operator = '*';
-        int answer = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an operation : ");
+        String operator = sc.next();
+        System.out.println("Enter number 1: ");
+        int var1 =sc.nextInt();
+        System.out.println("Enter number 2: ");
+        int var2 = sc.nextInt();
 
-        if(operator == '+'){
-            answer = var1 + var2;
-        }else if(operator == '-'){
-            answer = var1 - var2;
-        }else if(operator == '*'){
-            answer = var1 * var2;
-        }else if(operator == '/'){
-            answer = var1 / var2;
-        }else{
-            System.out.println("invalid operator");
+        int answer = 0;
+        switch (operator) {
+            case "+":
+                answer = var1 + var2;
+                break;
+            case "-":
+                answer = var1 - var2;
+                break; 
+            case "*":
+                answer = var1 * var2;
+                break; 
+            case "/":
+                answer = var1 / var2;
+                break; 
+            default:
+                System.out.println("Invalid Error");;
         }
         System.out.println(answer);
     }
